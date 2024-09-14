a8291130b33a2a20fecffce5553f0060d5c2fb23
jordan wakelam <jwakelams@gmail.com>
2024-09-14 21:18:31 +0200

    sauvegarde de l'original avant écrasement avec nouveau départ

--- site_cv/src/App.js
+++ site_cv/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+function Header() {
+  const title = "la maison jungle.";
+  return (
+    <header>
+      <h1>{title.toUpperCase()}</h1>
+    </header>
+  );
+}
+
+function Description() {
+  return (
+    <p>Ici achetez toutes les plantes dont vous avez toujours rêvé 🌵🌱🎍</p>
+  );
+}
+
+function Cart() {
+  const plantes = [
+    { nom: "monstera", prix: 8 },
+    { nom: "liere", prix: 10 },
+    { nom: "bouquet de fleurs", prix: 15 },
+  ];
+
+  let component = "";
+
+  for (let i = 0; i < plantes.length; i++) {
+    
+    component += <li>plantes[i].nom : plantes[i].prix</li>;
+  }
+
+  return <ul>{component}</ul>;
+}
+
+function Banner() {
+  return (
+    <>
+      <Header />
+      <Description />
+      <Cart />
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Banner />
     </div>
   );
 }
